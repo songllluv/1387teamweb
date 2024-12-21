1ba83940fb79d57c35ebbbc9b07e9883e6224136
YXY317Coder <127300704+Name317@users.noreply.github.com>
2024-12-21 19:12:42 +0800

    Update script.js

--- lbp/script.js
+++ lbp/script.js
@@ -78,9 +78,9 @@ function dob(){
 		var d0 = JSON.parse(pt('get','speak_thing_' + String(i)));
 		var d = d0['speak_thing_' + String(i)];
 		var repo = d.split(" ");
-		for (var i = 0;i < repo.length;i++){
-			if (repo[i][0] === '@'){
-				repo[i] = '<a href = "https://name317.github.io/1387teamweb/users/?username=' + repo[i].substring(1,repo[i].length)  + '">' + repo[i] + '</a>';
+		for (var j = 0;j < repo.length;j++){
+			if (repo[j][0] === '@'){
+				repo[j] = '<a href = "https://name317.github.io/1387teamweb/users/?username=' + repo[j].substring(1,repo[j].length)  + '">' + repo[j] + '</a>';
 			}
 		}
 		app('<a href = "https://name317.github.io/1387teamweb/users/?username=' + dd + '">' + dd + '</a> : ' + repo.join(''));
